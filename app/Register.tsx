41938e9d73bff56af6f27cba26e0ab364b632e84
DanhCaTuanNgoc <127586212+DanhCaTuanNgoc@users.noreply.github.com>
2025-01-24 00:27:09 +0700

    Delete app/Register.tsx

--- app/Register.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text, TextInput, Button } from "react-native";
-
-export default function Register() {
-    return (
-        <View>
-            <Text>Register</Text>
-        </View>
-    );
-}
